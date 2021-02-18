f5f038b06b40d50857a17081c9972f4a1df3cc11
taoDahuyou <70084803+123taojiale@users.noreply.github.com>
2021-02-18 17:19:40 +0800

    Update waterfall.js
    首次加载图片溢出问题...
    原因分析:
            一开始 页面是没有出现滚动条的 option.container.clientWidth 获取的就是没有出现滚动条的时候的值
            但是我们给 容器设置的 width: 90%; 它是等到页面加载完后, 才渲染上去的, 此时若一个屏幕展示不了所有的图片
            就会出现滚动条, 90% 是不考虑滚动条的尺寸的 所以实际得到的值会偏小 就会出现图片溢出的效果

--- plugin/waterfall.js
+++ plugin/waterfall.js
@@ -1,90 +1,98 @@
 if (!window.myPlugin) {
     window.myPlugin = {};
 }
-/**
- * 创建一个图片瀑布流
- */
+
 window.myPlugin.createWaterFall = function (option) {
-    var defaulOption = {
-        minGap: 10, //最小间隙
-        imgSrcs: [], //图片路径的数组
-        imgWidth: 220, //单张图片的宽度
-        container: document.body //容器
+    let defaultOption = {
+        minGap: 10, // 图片之间的最小间隙
+        imgSrcs: [], // 所有图片路径信息
+        imgWidth: 220, // 单张图片的宽度
+        container: document.body // 默认的图片容器是 body
     };
-    var option = Object.assign({}, defaulOption, option);
-    var imgs = []; //存放所有的图片dom对象
 
-    //处理父元素
-    handleParent();
-    //创建图片元素
-    createImgs();
+    option = Object.assign({}, defaultOption, option);
 
-    //窗口尺寸变化事件
-    var debounce = myPlugin.debounce(setImgPosition, 300)
-    window.onresize = debounce;
+    let imgs = []; // 用于存放 img 对象
+    // console.log(option);
 
+    handleParent(); // 确保容器是相对定位
+    createImgs(); // 创建图片元素
+
+    let debounce = myPlugin.debounce(setImgPosition, 300);
+    window.onresize = debounce; // 适应页面尺寸的变化
     /**
-     * 设置每一张图片的坐标
-     */
-    function setImgPosition() {
-        var info = getHorizontalInfo();
-        var arr = new Array(info.number); //存放每一列下一张图片的top值
-        arr.fill(0);
-        imgs.forEach(function (img) {
-            //设置图片的坐标
-            var minTop = Math.min.apply(null, arr);
-            img.style.top = minTop + "px";
-            var index = arr.indexOf(minTop); //找到对应的列编号
-            arr[index] += img.clientHeight + info.gap;
-            //横坐标
-            img.style.left = index * (option.imgWidth + info.gap) + "px";
-        });
-        //设置容器高度
-        var maxTop = Math.max.apply(null, arr);
-        option.container.style.height = maxTop - info.gap + "px";
-    }
-
-    /**
-     * 得到图片水平方向上的信息
-     */
-    function getHorizontalInfo() {
-        var obj = {};
-        //容器宽度
-        obj.containerWidth = option.container.clientWidth;
-        //计算一行图片的数量
-        obj.number = (obj.containerWidth + option.minGap) / (option.imgWidth + option.minGap);
-        obj.number = Math.floor(obj.number); //每行的图片只能少，不能多
-        //计算水平空隙
-        obj.gap = (obj.containerWidth - obj.number * option.imgWidth) / (obj.number - 1);
-        return obj;
-    }
-
-    /**
-     * 创建图片
+     * 向页面中插入图片
      */
     function createImgs() {
-        var debounce = myPlugin.debounce(setImgPosition, 30);
-        // 循环图片路径数组
-        for (var i = 0; i < option.imgSrcs.length; i++) {
-            var img = document.createElement("img");
-            img.src = option.imgSrcs[i];
+        let debounce = myPlugin.debounce(setImgPosition, 30);
+        option.imgSrcs.forEach(src => {
+            let img = new Image();
+
+            img.src = src;
             img.style.width = option.imgWidth + "px";
             img.style.position = "absolute";
-            img.style.transition = ".5s"; // 实现过渡
+            img.style.transition = "0.3s";
             imgs.push(img);
-            img.onload = debounce; // 函数防抖
             option.container.appendChild(img);
+            img.onload = debounce;
+        });
+    }
+
+    /**
+     * 图片元素是绝对定位 其父级必须得是相对定位
+     */
+    function handleParent() {
+        let style = getComputedStyle(option.container);
+        if (style.position === 'static') {
+            option.container.style.position = "relative";
         }
     }
 
     /**
-     * 处理父元素，因为图片都是绝对定位，父元素必须是一个定位元素
+     * 设置页面中的图片的位置
      */
-    function handleParent() {
-        //如果父元素不是定位元素，则将其变为相对定位
-        var style = getComputedStyle(option.container);
-        if (style.position === "static") {
-            option.container.style.position = "relative";
-        }
+    function setImgPosition() {
+        let info = getHorizontalInfo();
+        let arr = new Array(info.number).fill(0); // 每一列的高度值
+        imgs.forEach(img => {
+            // 设置图片的纵坐标
+            let minTop = Math.min.apply(null, arr);
+            img.style.top = minTop + "px";
+            // 设置图片的横坐标
+            let index = arr.indexOf(minTop);
+            img.style.left = index * (option.imgWidth + info.gap) + "px";
+            // 更新数组
+            arr[index] += info.gap + img.clientHeight;
+        });
+        // 设置容器的高度
+        let maxTop = Math.max.apply(null, arr);
+        option.container.style.height = maxTop + "px";
+        console.log(info);
+    }
+
+    /**
+     * 获取水平方向上的相关信息
+     * containerWidth   此时水平方向上的长度值
+     * number           该长度所能容纳的图片量(取小)
+     * gap              图片与图片之间的间隙值
+     */
+    function getHorizontalInfo() {
+        let obj = {};
+        obj.containerWidth = option.container.clientWidth;
+        obj.number = Math.floor((obj.containerWidth + option.minGap) / (option.imgWidth + option.minGap));
+        obj.gap = (obj.containerWidth - option.imgWidth * obj.number) / (obj.number - 1)
+
+        /* console.log(`
+        \n容器宽度: ${obj.containerWidth}, (第一次加载时会发现 该值比实际的要偏大)
+        \n间距: ${obj.gap},
+        \n图片数量: ${obj.number},
+        \n所有图片的宽度: ${option.imgWidth * obj.number},
+        \n所有间距的宽度: ${obj.gap * (obj.number - 1)}
+        `); */
+        /* 原因分析:
+        一开始 页面是没有出现滚动条的 option.container.clientWidth 获取的就是没有出现滚动条的时候的值
+        但是我们给 容器设置的 width: 90%; 它是等到页面加载完后, 才渲染上去的, 此时若一个屏幕展示不了所有的图片
+        就会出现滚动条, 90% 是不考虑滚动条的尺寸的 所以实际得到的值会偏小 就会出现图片溢出的效果 */
+        return obj;
     }
 }
